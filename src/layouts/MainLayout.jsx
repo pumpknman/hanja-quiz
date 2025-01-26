a19ade56c7823a3fb6b962078b78c85bd468ed55
PMKN <shameman@naver.com>
2025-01-26 13:00:23 +0900

    Update MainLayout.jsx
    하단에 광고 영역 추가

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -16,15 +16,16 @@ function MainLayout({ children }) {
                 bottom: 0,
                 background: "linear-gradient(#0F1214, #0C1A27)",
                 display: "flex",
-                justifyContent: "center",
+                flexDirection: "column", // 위아래 배치를 위해 column 추가
+                justifyContent: "space-between", // 콘텐츠와 하단 광고 간 간격 유지
                 alignItems: "center",
                 padding: { xs: 1, md: 0 },
                 boxSizing: "border-box",
                 height: "100vh",
                 overflow: "hidden",
-
             }}
         >
+            {/* 메인 콘텐츠 영역 */}
             <Box
                 sx={{
                     display: "flex",
@@ -32,7 +33,7 @@ function MainLayout({ children }) {
                     alignItems: "center",
                     width: "100%",
                     maxWidth: "1200px",
-
+                    flex: 1, // 콘텐츠 영역이 가능한 공간을 차지하도록 설정
                 }}
             >
                 {/* 왼쪽 광고 영역 */}
@@ -61,7 +62,7 @@ function MainLayout({ children }) {
                 <Box
                     sx={{
                         width: { xs: "95%", md: 815 },
-                        height: { xs: "calc(100vh - 20px)", md: 815 },
+                        height: { xs: "calc(100vh - 20px - 60px)", md: 815 }, // 하단 광고 높이만큼 차감
                         maxWidth: 815,
                         maxHeight: 815,
                         backgroundColor: "#0F1214",
@@ -74,7 +75,6 @@ function MainLayout({ children }) {
                         boxSizing: "border-box",
                         overflow: "hidden",
                         position: "relative",
-                        backgroundColor: "#0F1214",
                     }}
                 >
                     {/* ResultPage의 경우 별도 레이아웃 적용 */}
@@ -86,7 +86,6 @@ function MainLayout({ children }) {
                                 display: "flex",
                                 justifyContent: "center",
                                 alignItems: "center",
-
                             }}
                         >
                             {children}
@@ -117,6 +116,32 @@ function MainLayout({ children }) {
                         browsingtopics
                     ></iframe>
                 </Box>
+            </Box>
+
+            {/* 하단 고정 광고 영역 (모바일에서만 표시) */}
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "60px",
+                    display: { xs: "flex", md: "none" }, // 모바일에서만 표시
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#1B1F24",
+                    borderTop: "1px solid #2C2F34",
+                    position: "fixed", // 고정된 위치
+                    bottom: 0,
+                    left: 0,
+                }}
+            >
+                <iframe
+                    src="https://ads-partners.coupang.com/widgets.html?id=832578&template=carousel&trackingCode=AF9925383&subId=&width=320&height=60&tsource="
+                    width="320"
+                    height="60"
+                    frameBorder="0"
+                    scrolling="no"
+                    referrerPolicy="unsafe-url"
+                    browsingtopics
+                ></iframe>
             </Box>
         </Box>
     );
